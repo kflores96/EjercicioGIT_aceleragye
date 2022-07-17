9cb5041c8df1fe56de7bb38f2d1515c29c3539ba
kflores96 <107453192+kflores96@users.noreply.github.com>
2022-07-17 13:57:30 -0500

    metodos

---
+++ Modulo_6/metodos.js
@@ -0,0 +1,44 @@
+/* let frutas = ["Manzana", "pera", "melon"];
+// Recorrer un array
+frutas.forEach(
+    function (frutas) {
+        console.log(frutas);
+    }
+    ) */
+
+
+/* añadir un elemento al FINAL de un array */
+/* let frutas = ["Manzana", "pera", "melon"];
+let nuevoItem = frutas.push("pera")
+console.log(frutas); */
+
+/* elminar el ULTIMO elemento de un array */
+/* let frutas = ["Manzana", "pera", "melon", "pera"] ;
+let deleteNewItem = frutas.pop("pera")
+console.log(frutas); */
+
+/* añadir un elemento al PRINCIPIO de un array */
+/* let frutas = ["Manzana", "pera", "melon", "pera"]
+let nuevoItemFirts = frutas.unshift ("mora")
+console.log(frutas); */
+
+/* eliminar un elemento al PRINCIPIO  de un array */
+/* let frutas = ["Manzana", "pera", "melon", "pera"]
+let borrarItem = frutas.shift("Manzana");
+console.log(frutas); */
+
+/* buscar posicion*/
+/* et frutas = ["Manzana", "pera", "melon", "pera"]
+let pos = frutas.indexOf("Manzana")
+console.log(frutas); */
+
+/* eliminar un unico elemento desde su ubicacion */
+/* let frutas = ["Manzana", "pera", "melon", "pera"]
+let ubica = frutas.splice(1,1)
+console.log(frutas); */
+
+/* copiar un array */
+/* let frutas = ["Manzana", "pera", "melon", "pera"]
+let copia = frutas.slice();
+console.log(frutas); */
+
